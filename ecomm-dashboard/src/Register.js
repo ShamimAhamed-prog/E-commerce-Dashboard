346c4853cfc2c7890dc28278bbc6679aa1fc7913
ShamimAhamed-prog <shamim.aiub2@gmail.com>
2021-08-19 16:46:29 +0600

    forth

--- ecomm-dashboard/src/Register.js
+++ ecomm-dashboard/src/Register.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Register = () => {
-    return (
+function Register () {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function signUp()
+    {
+        let item ={name,email,password}
+             console.warn(item)
+    
+    let result= await fetch("http://localhost:8000/api/register",{
+        method:'POST',
+        body:JSON.stringify(item),
+        headers:{
+            "Content-Type": 'application/json',
+            "Accept":'application/json'     
+        }
+    })
+    result =await result.json()
+    console.warn("result",result)
+}
+         return (
+
         <div>
-            <h1> Register now</h1>
+                <h3>Sign Up</h3>
+
+                <div className=" col-sm-6 offset-sm-3">
+                    <label>Name</label>
+                    <input type="text" value ={name} onChange={(e)=>setName(e.target.value)} className="form-control" placeholder="name" />
+                    <br />
+                    <label>Email address</label>
+                    <input type="email"  value ={email}onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="Enter email" />
+                    <br />
+                    <label>Password</label>
+                    <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)}className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" onClick={signUp} className="btn btn-primary btn-block">Sign Up</button>
         </div>
     )
 }
